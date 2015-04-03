6532c926b1ce1c1c7179760279b1710af4f4af6d
joshaddington <josh.addington@gmail.com>
2015-04-03 00:08:35 -0400

    added templates, updated blog app

--- wsgi/blog/urls.py
+++ wsgi/blog/urls.py
@@ -1,10 +1,12 @@
 from . import views
-from django.conf import settings
-from django.conf.urls import patterns, include, url
-from django.contrib import admin
-admin.autodiscover()
-
+from django.conf.urls import patterns, url
 
 urlpatterns = patterns('',
-    url(r'^$', views.index),
+        url(r'^$', views.post_list),
+        url(r'^post/(?P<pk>[0-9]+)/$' , views.post_detail),
+        url(r'^post/new/$', views.post_new, name='post_new'),
+        url(r'^post/(?P<pk>[0-9]+)/edit/$', views.post_edit, name='post_edit'),
+        url(r'^post/(?P<pk>[0-9]+)/publish/$', views.post_publish, name='post_publish'),
+        url(r'^post/(?P<pk>[0-9]+)/delete/$', views.post_delete, name='post_delete'),
+        url(r'^post/drafts/$', views.post_draft_list, name='post_draft_list'),
 )
--- wsgi/blog/views.py
+++ wsgi/blog/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
+from django.contrib.auth.decorators import login_required
 from .models import Post
 from .forms import PostForm, CommentForm
 
@@ -26,3 +27,55 @@ def post_detail(request, pk):
         return render(request, 'blog/post_detail.html', {'post': post, 'projects_length': projects_length, 'form': form})
 
 
+@login_required
+def post_new(request):
+        if request.method == 'POST':
+                form = PostForm(request.POST)
+                if form.is_valid():
+                        post = form.save(commit=False)
+                        post.author = request.user
+                        post.save()
+                        # This saves the project relation
+                        form.save_m2m()
+                        return redirect('blog.views.psot_detail', pk=post.pk)
+        else:
+                form = PostForm()
+        return render(request, 'blog/post_edit.html', {'form': form})
+
+
+@login_required
+def post_edit(request, pk):
+        post = get_object_or_404(Post, pk=pk)
+        if request.method == "POST":
+                form = PostForm(request.POST, instance=post)
+                if form.is_valid():
+                        post = form.save(commit=False)
+                        post.author = request.user
+                        post.save()
+                        # This saves project relation
+                        form.save_m2m()
+                return redirect('blogging.views.post_detail', pk=post.pk)
+        else:
+                action = "Edit"
+                form = PostForm(instance=post)
+        return render(request, 'blog/post_edit.html', {'form': form, 'action': action})
+
+
+@login_required
+def post_draft_list(request):
+        posts = Post.objects.filter(published_date__isnull=True).order_by('created_date')
+        return render(request, 'blog/post_draft_list.html', {'posts': posts})
+
+
+@login_required
+def post_publish(request, pk):
+        post = get_object_or_404(Post, pk=pk)
+        post.publish()
+        return redirect('blogging.views.post_detail', pk=pk)
+
+
+@login_required
+def post_delete(request, pk):
+        post = get_object_or_404(Post, pk=pk)
+        post.delete()
+        return redirect('blogging.views.post_list')
