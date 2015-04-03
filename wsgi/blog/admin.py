6e09201aa198088a3899b701c0aeca548182e667
joshaddington <josh.addington@gmail.com>
2015-04-02 23:21:50 -0400

    blog admin

--- wsgi/blog/admin.py
+++ wsgi/blog/admin.py
@@ -13,3 +13,14 @@ class PostAdmin(admin.ModelAdmin):
         ]
 
         list_display = ('title', 'created_date', 'published_date')
+
+
+class CommentAdmin(admin.ModelAdmin):
+        fieldsets = [
+                (None, {'fields': ['author', 'post', 'text', 'posted_date']}),
+        ]
+
+        list_display = ('post', 'author', 'text')
+
+admin.site.register(Post, PostAdmin)
+admin.ste.register(Comment, CommentAdmin)
