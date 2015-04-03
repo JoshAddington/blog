63aa2c6d8040f49f0641ff6bac2425a438ad5354
joshaddington <josh.addington@gmail.com>
2015-04-03 11:30:46 -0400

    adjust application code

--- wsgi/openshift/settings.py
+++ wsgi/openshift/settings.py
@@ -11,7 +11,9 @@ https://docs.djangoproject.com/en/1.7/ref/settings/
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+
 TEMPLATE_DIRS = (os.path.join(BASE_DIR, 'templates'),)
+print(TEMPLATE_DIRS)
 
 # Running on OpenShift ?
 ON_OPENSHIFT = False
--- wsgi/openshift/urls.py
+++ wsgi/openshift/urls.py
@@ -1,4 +1,4 @@
-from . import views
+from openshift import views
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 from django.conf.urls.static import static
