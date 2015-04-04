04a162b1722eed28efb9fba7ff64e93988e3a1a9
joshaddington <josh.addington@gmail.com>
2015-04-04 00:39:37 -0400

    fix cron

--- .openshift/cron/minutely/citibike.bash
+++ .openshift/cron/minutely/citibike.bash
@@ -2,4 +2,4 @@
 
 minute=$(date +%M)
 if [ $(($minute % 5)) -eq 0 ]; then
-        python "$OPENSHIFT_REPO_DIR"wsgi/citibike/update_bike_table.py
+        python "$OPENSHIFT_REPO_DIR"wsgi/update_bike_table.py
