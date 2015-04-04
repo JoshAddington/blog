e23732b977375b357e804e1df77e68093c62bba7
joshaddington <josh.addington@gmail.com>
2015-04-04 19:02:28 -0400

    fix order of urls, fix #2

--- wsgi/blog/urls.py
+++ wsgi/blog/urls.py
@@ -3,10 +3,11 @@ from django.conf.urls import patterns, url
 
 urlpatterns = patterns('',
         url(r'^$', views.post_list),
-        url(r'^post/(?P<slug>[-\w\d]+)/$' , views.post_detail),
+        url(r'^post/drafts/$', views.post_draft_list, name='post_draft_list'),
         url(r'^post/new/$', views.post_new, name='post_new'),
+        url(r'^post/(?P<slug>[-\w\d]+)/$' , views.post_detail),
         url(r'^post/(?P<slug>[-\w\d]+)/edit/$', views.post_edit, name='post_edit'),
         url(r'^post/(?P<slug>[-\w\d]+)/publish/$', views.post_publish, name='post_publish'),
         url(r'^post/(?P<slug>[-\w\d]+)/delete/$', views.post_delete, name='post_delete'),
-        url(r'^post/drafts/$', views.post_draft_list, name='post_draft_list'),
+
 )
