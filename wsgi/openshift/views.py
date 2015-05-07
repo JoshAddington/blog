1d37bd33b3bbfd2c262fe757c6e9c074b0d331ad
joshaddington <josh.addington@gmail.com>
2015-05-06 22:47:38 -0400

    add boroughs url

--- wsgi/openshift/views.py
+++ wsgi/openshift/views.py
@@ -1,3 +1,8 @@
+import os
+import json
+
+from django.conf import settings
+from django.http import JsonResponse
 from django.shortcuts import render
 
 
@@ -7,3 +12,7 @@ def about(request):
 
 def contact(request):
         return render(request, 'contact.html')
+
+
+def boroughs(request):
+        return JsonResponse(json.loads(open(os.path.join(settings.STATIC_ROOT, 'js', 'nyc.json')).read()))
