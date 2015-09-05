419fe9dae28cf48b979bc12f145d2b858ef5cd92
joshaddington <josh.addington@gmail.com>
2015-09-05 14:25:30 -0400

    Adds citibike model

--- mysite/citibike/models.py
+++ mysite/citibike/models.py
@@ -1,3 +1,34 @@
 from django.db import models
+import datetime
+
 
 # Create your models here.
+class Station(models.Model):
+        id = models.IntegerField(primary_key=True)
+        name = models.CharField(max_length=128, unique=True)
+        availableDocks = models.IntegerField()
+        latitude = models.DecimalField(max_digits=12, decimal_places=9)
+        longitude = models.DecimalField(max_digits=12, decimal_places=9)
+
+        def __str__(self):
+                return self.name
+
+        def __int__(self):
+                return self.id
+
+
+class UpdateTime(models.Model):
+        time = models.TimeField()
+        date = models.DateField(default=datetime.date.today)
+
+        def __str__(self):
+                return str(self.date + " " + self.time)
+
+
+class Bike(models.Model):
+        station = models.ForeignKey(Station, related_name='bikes')
+        number_of_bikes = models.IntegerField()
+        update = models.ForeignKey(UpdateTime, related_name='bike_update')
+
+        def __str__(self):
+                return str(self.id)
