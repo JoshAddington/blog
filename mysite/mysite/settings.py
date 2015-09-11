43ac5929ebe8a5fb0161c40fe448f48e4ea653df
joshaddington <josh.addington@gmail.com>
2015-09-11 16:48:06 -0400

    add citibike/static/ folder to static_dirs

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -130,6 +130,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, '..', 'www', 'static')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'citibike', 'static'),
 )
 
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
