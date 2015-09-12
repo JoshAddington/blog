60ff8ae48ef25b29d7a1e68df70346da0ee567ec
joshaddington <josh.addington@gmail.com>
2015-09-11 22:34:41 -0400

    add citibike api urls

--- mysite/citibike/urls.py
+++ mysite/citibike/urls.py
@@ -1,4 +1,10 @@
 from . import views
 from django.conf.urls import url, patterns
 
-urlspatterns = patterns('',)
+urlpatterns = patterns('',
+    url(r'^boroughs/$', views.boroughs),
+    # url(r'^map/$', views.citibike_map),
+    url(r'^stations/$', views.station_collection),
+    url(r'^stations/(?P<pk>[0-9]+)/$', views.station_detail),
+    url(r'^stations/(?P<pk>[0-9]+)/bikes$', views.station_bikes),
+)
