7961361edfddb242ce46fd7256ef44cc8bee8213
joshaddington <josh.addington@gmail.com>
2015-10-04 17:45:09 -0400

    add resume url and view for resume download

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -15,6 +15,6 @@ urlpatterns = patterns('',
         url(r'^accounts/logout/$', 'django.contrib.auth.views.logout', {'next_page': '/'}),
         url(r'^admin/', include(admin.site.urls)),
         url(r'^contact/$', views.contact),
-        # url(r'^citibike/', include('citibike.urls')),
+        url(r'^resume/', views.resume),
         url('', include('django.contrib.auth.urls', namespace='auth')),
 )
